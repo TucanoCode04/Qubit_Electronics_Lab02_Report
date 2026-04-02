fab01f4821a1c9f5d9173f7276c93760446e4c0d
klakson1306 <leonardoirdy@gmail.com>
2026-04-02 10:38:37 +0200

    passband

--- leopart/analysis_pb.py
+++ leopart/analysis_pb.py
@@ -13,13 +13,15 @@ def get_stats(nome,m,n):
 
 get_stats("S22",1,1)
 get_stats("S12",0,1)
+get_stats("S11",0,0)
+get_stats("S21",1,0)
 
-s21_db = net.s_db[:, 1, 1]
-idx_peak = np.argmin(s21_db)
+s21_db = net.s_db[:, 0, 1]
+idx_peak = np.argmax(s21_db)
 f_center = net.f[idx_peak]
 val_peak = s21_db[idx_peak]
 
-target = val_peak + 3
+target = val_peak - 3
 idx_l = np.argmin(np.abs(s21_db[:idx_peak] - target))
 idx_h = np.argmin(np.abs(s21_db[idx_peak:] - target)) + idx_peak
 
@@ -27,6 +29,21 @@ f_l, f_h = net.f[idx_l], net.f[idx_h]
 bw = f_h - f_l
 q_factor = f_center / bw
 
+print(f"f low: {f_l/1e6:.2f} MHz")
+print(f"f high: {f_h/1e6:.2f} MHz")
 print(f"f peak: {f_center/1e6:.2f} MHz")
 print(f"bandwidth: {bw/1e6:.2f} MHz")
 print(f"quality factor: {q_factor:.2f}")
+
+#calculating the match. bw at -10db
+s11_db = net.s_db[:, 0, 0]
+under_11 = np.where(s11_db < -10)[0]
+if len(under_11)>0:
+    f_match_bw = net.f[under_11[-1]] - net.f[under_11[0]]
+    print(f"Matching bandwidth S11 at -10dB: {f_match_bw/1e6:.2f} MHz")
+
+s22_db = net.s_db[:, 1, 1]
+under_22 = np.where(s22_db < -10)[0]
+if len(under_22)>0:
+    f_match_bw = net.f[under_22[-1]] - net.f[under_22[0]]
+    print(f"Matching bandwidth S22 at -10dB: {f_match_bw/1e6:.2f} MHz")
